f4a4fdfe889f599a265dbb4ba702da99f438402d
Khalid AlNemr <121998410+Osiris-00@users.noreply.github.com>
2023-04-03 15:41:41 +0200

    Add files via upload
    This isn't completed yet. I'm still new to programing and I'm not quite familiar with how C++ works. So far, it will only ask you to enter two numbers and it will add them. 
    I will update this as I learn. Stay tuned!

---
+++ Calculator.cpp
@@ -0,0 +1,32 @@
+// Project4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+void addition() 
+{
+	std::cout << "Please enter an integer: ";
+	int userinput{};
+	int userinput2{};
+	std::cin >> userinput;
+	std::cin >> userinput2;
+	std::cout << "The sum of " << userinput << " and " <<
+		userinput2 << " is " << userinput + userinput2;
+
+}
+
+
+
+int main(){
+	addition();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
