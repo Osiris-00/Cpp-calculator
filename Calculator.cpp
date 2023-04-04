1251a322e9deffd5daa085dc52cdf980121ea42c
Khalid AlNemr <121998410+Osiris-00@users.noreply.github.com>
2023-04-04 16:28:03 +0200

    New and updated version.
    Now it can add, subtract, multiply, or divide any 2 integers. Hurrayy!! O:

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,23 +1,78 @@
-// Project4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
-void addition() 
+
+int getInput() {
+    int input{};
+    std::cin >> input;
+    return(input);
+}
+void addition()
 {
-	std::cout << "Please enter an integer: ";
-	int userinput{};
-	int userinput2{};
-	std::cin >> userinput;
-	std::cin >> userinput2;
-	std::cout << "The sum of " << userinput << " and " <<
-		userinput2 << " is " << userinput + userinput2;
+    std::cout << "Type the 2 numbers you would like to add\n";
+    int x{ getInput() };
+    int y{ getInput() };
+    std::cout << "The sum of " <<x << " and " << y << " is " << x + y;
 
 }
 
+void subtraction()
+{
+    std::cout << "Type the 2 numbers you would like to subtract\n";
+    int x{ getInput() };
+    int y{ getInput() };
+    std::cout << x << "-" << y << " = " << x - y;
 
+}
+void multiplication()
+{
+    std::cout << "Type the 2 numbers you would like to multiply\n";
+    int x{ getInput() };
+    int y{ getInput() };
+    std::cout << x << "*" << y << " = " << x * y;
+}
+void division ()
+{
+    std::cout << "Type the 2 numbers you would like to divide\n";
+    int x{ getInput() };
+    int y{ getInput() };
+    std::cout << x << "/" << y << " = " << x / y;
+}
 
-int main(){
-	addition();
+int main()
+{
+    std::cout << "Hello, welcome to this primitive calculator program!";
+    std::cout << "Please choose what you want to do\n";
+    std::cout << "[1] Add 2 integers\n[2] Subtract 2 integers \n[3] Multiply 2 integers\n[4] Divide 2 integers\n";
+    int menuChoice{ getInput() };
+    std::cout << "You chose: " << menuChoice << std::endl;
+    if (menuChoice == 1)
+    {
+        std::cout << "Running addition function... \n";
+        addition();
+    }
+    else if (menuChoice == 2)
+    {
+        std::cout << "Running subtraction function... \n";
+        subtraction();
+
+    }
+
+    else if (menuChoice == 3)
+    {
+        std::cout << "Running multiplication function... \n";
+        multiplication();
+    }
+    else if (menuChoice == 4)
+    {
+        std::cout << "Running division function... \n";
+        division();
+    }
+    else {
+
+        return 0;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
